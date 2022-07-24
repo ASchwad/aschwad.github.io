74d753fd85134e8651dccfbe87f64993da09ca2a
ASchwad <a.schoenenwald@gmx.de>
2022-07-24 16:02:38 +0200

    feat: add project references

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import './Projects.css';
 import { projects } from './metadata';
 import useWindowDimensions from '../../hooks/getWindowDimensions';
+import { Button } from '@mui/material';
+
+type IReference = {
+  title: string,
+  url: string,
+}
 
 type IProject = {
   key: string,
@@ -9,6 +15,7 @@ type IProject = {
   description: string,
   preview_image: string,
   type: string,
+  references: IReference[]
 }
 
 export default function Projects() {
@@ -29,15 +36,37 @@ export default function Projects() {
       }
     </div>
   };
+  function ProjectReferences({ references }: { references: IReference[] | undefined }) {
+    if (references === undefined) {
+      return null
+    }
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        {references.map(reference => {
+          return (
+            <Button className='reference-button' variant="contained" href={reference.url} target="_blank" rel="noopener noreferrer" sx={{
+              backgroundColor: "#fdf420", color: "black", marginRight: 2, ':hover': {
+                bgcolor: 'black',
+                color: '#fdf420',
+              },
+            }}>
+              {reference.title}
+            </Button>
+          )
+        })}
+      </div>
+    )
+  }
   function ProjectDetails({ project }: { project: IProject | undefined }) {
     return (
       <div key={project?.key} className='project-details' style={{ backgroundColor: 'transparent' }}>
-        <h6>{(project?.type + " project").toUpperCase()}</h6>
+        <h6 style={{ textTransform: "capitalize" }}>{(project?.type) + " Project"}</h6>
         <div style={{ color: "white", paddingLeft: 0 }}>
           <h4>{project?.title}</h4>
           <p>{project?.description}</p>
         </div>
         <img alt={project?.title} src={require("./images/" + project?.preview_image)} style={{ width: "100%", marginBottom: 15 }} />
+        <ProjectReferences references={project?.references} />
       </div >
     )
   };
--- src/components/Projects/metadata.ts
+++ src/components/Projects/metadata.ts
@@ -5,13 +5,33 @@ export const projects = [
     title: "Software products at Futuro Farming",
     description: "Multiple (mobile and web) apps and the underlying backend infrastructure within the last 4 years of building our own company.",
     type: "key",
+    references: [
+      {
+        title: "Android",
+        url: "https://play.google.com/store/apps/details?id=com.cmnativeapp"
+      },
+      {
+        title: "iOS",
+        url: "https://apps.apple.com/de/app/calf-monitoring/id1510538724"
+      }
+    ]
   },
   {
     key: "lineage",
     preview_image: "lineage.png",
     title: "Data Lineage at BMW Group",
-    description: "Researched and implemented a tool (proof of concept) for BMW Group's data lake team to provide a better understanding of the data that is being used in the BMW Group's business. Published via a paper and blog posts.",
+    description: `Researched and implemented a proof of concept for BMW Group's data lake team to provide a better understanding of the data that is being used in the BMW Group's business. We published our results via paper and blog posts.`,
     type: "key",
+    references: [
+      {
+        title: "Blog",
+        url: "https://medium.com/@alex.schoenenwald/fishing-for-data-lineage-within-an-aws-based-data-lake-908a59b43574"
+      },
+      {
+        title: "Paper",
+        url: "https://link.springer.com/article/10.1007/s13222-021-00387-7"
+      }
+    ]
   },
   {
     key: "gtc",
@@ -19,6 +39,12 @@ export const projects = [
     title: "Grand Theft Candy (iOS App with AR)",
     description: "Developed an Augmented Reality iOS game as assignment at the University of Munich. Place game field in the real world and collect candy to win - but don't get caught by the police!",
     type: "minor",
+    references: [
+      {
+        title: "Github",
+        url: "https://github.com/ASchwad/GTC"
+      },
+    ]
   },
   {
     key: "lyza",
@@ -26,6 +52,16 @@ export const projects = [
     title: "Lyza: Spotify Playlist analyzer",
     description: "Analyze and visualize Spotify playlist information via a React Web App using the public Spotify API.",
     type: "minor",
+    references: [
+      {
+        title: "Github",
+        url: "https://github.com/ASchwad/spotify-react-app"
+      },
+      {
+        title: "Web App",
+        url: "https://aschwad.github.io/spotify-react-app/"
+      },
+    ]
   },
   {
     key: "route-builder",
@@ -33,6 +69,16 @@ export const projects = [
     title: "Route builder using vanilla leaflet",
     description: "Enables you to plan a route for your next session of cross-country running. You can place waypoints on the map which will form your route.",
     type: "minor",
+    references: [
+      {
+        title: "Github",
+        url: "https://github.com/ASchwad/route-builder"
+      },
+      {
+        title: "Web App",
+        url: "https://aschwad.github.io/route-builder/"
+      },
+    ]
   },
   {
     key: "translations",
@@ -40,6 +86,16 @@ export const projects = [
     title: "React Native translation tooling",
     description: "Showcase react native project to demonstrate a workflow to transform an excelsheet of translations to a JSON file and leverage it in the application source code.",
     type: "minor",
+    references: [
+      {
+        title: "Github",
+        url: "https://github.com/ASchwad/react-translation-workflows"
+      },
+      {
+        title: "Blog",
+        url: "https://medium.com/@alex.schoenenwald/workflow-from-excel-translations-to-translated-keys-in-your-react-native-app-16ddf67c897c"
+      },
+    ]
   },
   {
     key: "dht",
@@ -47,5 +103,11 @@ export const projects = [
     title: "DHT11: Humidity and temperature sensor",
     description: "Raspberry PI with a DHT11 humidity and temperature sensor provided insights in our apartment climate via Google Firestore and Telegram Push Notification API.",
     type: "minor",
+    references: [
+      {
+        title: "Github",
+        url: "https://github.com/ASchwad/Smarthome-Raspi-DHT11"
+      },
+    ]
   },
 ]
